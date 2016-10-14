995c595b2320b71ef8e34664e702842653774834
Kevin Zhang <zarknight@gmail.com>
2016-10-14 16:04:03 +0800

    use bluebird Promise

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,20 +1,22 @@
+import Promise from '../libs/bluebird.min'
 import util from '../utils/util'
 
 const app = getApp()
 
 function getUserInfo() {
   let userInfo = app.globalData.userInfo
+
   if (userInfo) {
-    return new Promise((resolve, reject) => resolve(userInfo))
-  } else {
-    return util.wxPromisify(wx.login)().then(() => {
-      return util.wxPromisify(wx.getUserInfo)()
-    }).then((res) => {
-      userInfo = res.userInfo
-      app.globalData.userInfo = userInfo
-      return userInfo
-    })
+    return Promise.resolve(userInfo)
   }
+
+  return util.wxPromisify(wx.login)().then(() => {
+    return util.wxPromisify(wx.getUserInfo)()
+  }).then((res) => {
+    userInfo = res.userInfo
+    app.globalData.userInfo = userInfo
+    return userInfo
+  })
 }
 
 module.exports = {
